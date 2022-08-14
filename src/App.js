9f529571d9f64f09260e7eccd062f58795e21006
Fillip Molodtsov <greatdrummer1@gmail.com>
2022-08-14 12:36:33 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
 import React from 'react'
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
 } from 'react-router-dom'
-import { AddPostForm } from './app/features/posts/AddPostForm'
-import { PostsList } from './app/features/posts/PostsList'
-import { SinglePostPage } from './app/features/posts/SinglePostPage'
-import { EditPostForm } from './app/features/posts/EditPostForm'
+import {AddPostForm} from './app/features/posts/AddPostForm'
+import {PostsList} from './app/features/posts/PostsList'
+import {SinglePostPage} from './app/features/posts/SinglePostPage'
+import {EditPostForm} from './app/features/posts/EditPostForm'
 
-import { Navbar } from './app/Navbar'
+import {Navbar} from './app/Navbar'
+import {UsersList} from "./app/features/users/UsersList";
+import {UserPage} from "./app/features/users/UserPage";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <div className="App">
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <>
-              <AddPostForm />
-              <PostsList />
-              </>
-            )}
-          />
-          <Route exact path="/posts/:postId" component={SinglePostPage} />
-          <Route exact path="/editPost/:postId" component={EditPostForm} />
-          <Redirect to="/" />
-        </Switch>
-      </div>
-    </Router>
-  )
+    return (
+        <Router>
+            <Navbar/>
+            <div className="App">
+                <Switch>
+                    <Route
+                        exact
+                        path="/"
+                        render={() => (
+                            <>
+                                <AddPostForm/>
+                                <PostsList/>
+                            </>
+                        )}
+                    />
+                    <Route exact path="/posts/:postId" component={SinglePostPage}/>
+                    <Route exact path="/editPost/:postId" component={EditPostForm}/>
+                    <Route exact path="/users" component={UsersList}/>
+                    <Route exact path="/users/:userId" component={UserPage}/>
+                    <Redirect to="/"/>
+                </Switch>
+            </div>
+        </Router>
+    )
 }
 
 export default App
